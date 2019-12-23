ac617e0ad8bf970f2b32cffa1bd498687636edbb
0407chan <0407chan@naver.com>
2019-12-23 23:46:31 +0900

    Update main8958_OX퀴즈.cpp

--- boj/main8958_OX퀴즈.cpp
+++ boj/main8958_OX퀴즈.cpp
@@ -1,3 +1,12 @@
+/*
+* string에 대해 배웠다.
+* string을 사용하려면 include를 해야한다
+* string은 getline으로 읽어온다.
+* getline 전에 cin으로 읽어왔다면 cin.ignore로 그 전에있는 '\n'을 처리해줘야한다.
+* string을 index로 접근을 하면 char로 읽는다.
+* 연습하자
+*/
+
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
