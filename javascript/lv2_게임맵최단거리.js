482b727a9292b73230c0eaa54a37f58a9f2dde9d
이찬호 <chanho@woowahan.com>
2024-05-26 17:55:56 +0900

    feat: lv2_게임맵최단거리 bfs 풀이 - 기존에 방문 처리하는 부분을 굉장히 쉽게 하는것이 인상적 - bfs라서 처음 도달하는 곳이 최단거리인 것도 인상적 - JSON.stringify, JSON.parse로 매번 배열 깊은 복사 하지 않아도 됨 - queue 활용으로 굉장히 심플하게 방문 처리함

--- javascript/lv2_게임맵최단거리.js
+++ javascript/lv2_게임맵최단거리.js
@@ -1,50 +1,27 @@
-const dx = [0, 1, 0, -1]
-const dy = [1, 0, -1, 0]
-
 function solution(maps) {
-  let answer = Infinity
-  const endX = maps.length - 1
-  const endY = maps[0].length - 1
+  const n = maps[0].length - 1
+  const m = maps.length - 1
+  const queue = [[0, 0, 1]]
+  while (queue.length) {
+    const [x, y, count] = queue.shift()
+    if (x == n && y == m) return count //bfs라서 처음 도착한게 최단거리
+    if (!maps[y][x]) continue
+    maps[y][x] = false
 
-  const visitMap = JSON.parse(JSON.stringify(maps))
-  visitMap.forEach((arr) => arr.fill(false))
-
-  getAnswer(0, 0, 0, visitMap)
-  function getAnswer(x, y, count, visit) {
-    const newVisit = JSON.parse(JSON.stringify(visit))
-    if (
-      x < 0 ||
-      y < 0 ||
-      x > endX ||
-      y > endY ||
-      visit[x][y] ||
-      maps[x][y] === 0
-    ) {
-      return
-    }
-
-    newVisit[x][y] = true
-
-    if (x === endX && y === endY) {
-      answer = Math.min(answer, count + 1)
-      return
-    }
-
-    for (let i = 0; i < 4; i++) {
-      const xx = dx[i]
-      const yy = dy[i]
-      getAnswer(x + xx, y + yy, count + 1, newVisit)
-    }
+    if (x - 1 >= 0 && maps[y][x - 1]) queue.push([x - 1, y, count + 1])
+    if (x + 1 <= n && maps[y][x + 1]) queue.push([x + 1, y, count + 1])
+    if (y - 1 >= 0 && maps[y - 1][x]) queue.push([x, y - 1, count + 1])
+    if (y + 1 <= m && maps[y + 1][x]) queue.push([x, y + 1, count + 1])
   }
 
-  return answer === Infinity ? -1 : answer
+  return -1
 }
 
 const maps = [
   [1, 0, 1, 1, 1],
-  [0, 0, 1, 1, 1],
+  [1, 0, 1, 1, 1],
   [1, 1, 1, 1, 1],
   [1, 1, 1, 0, 0],
-  [1, 1, 1, 0, 1]
+  [1, 1, 1, 1, 1]
 ]
 console.log(solution(maps)) // 11
